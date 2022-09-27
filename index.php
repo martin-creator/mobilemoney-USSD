5f9530080ce7d6b3a5b27f9c411df4a57638b97b
martin-creator <martinlubowa@outlook.com>
2022-09-27 18:58:51 +0300

    Added logic to handle registered or unregistered users' menu

--- index.php
+++ index.php
@@ -1,19 +1,35 @@
 <?php
 
 // https://4816-197-239-4-211.eu.ngrok.io/momo/index.php
+
+include_once 'menu.php';
+
 // Read the variables sent via POST from our API
-$sessionId   = $_POST["sessionId"];
+$sessionId   = $_POST["sessionId"]; // id that identifies each users session * one user can have many sessions
 $serviceCode = $_POST["serviceCode"];
 $phoneNumber = $_POST["phoneNumber"];
-$text        = $_POST["text"];
+$text        = $_POST["text"]; // this is what the user send back
 
-if ($text == "") {
-    // This is the first request. Note how we start the response with CON
-    $response  = "CON What would you want to check \n";
-    $response .= "1. My Account \n";
-    $response .= "2. My phone number";
+$isRegistered = true;
+$menu = new Menu($text, $sessionId);
 
-} else if ($text == "1") {
+if ($text == "" && !$isRegistered) {
+    // user is registered and string is empty. Note how we start the response with CON
+    $menu->mainMenuRegistered();
+
+} else if( $text == "" &&  $isRegistered){
+  // user is not registered and string is empty
+  $menu->mainMenuUnRegistered();
+
+}else if( $isRegistered ){
+// user is not registered and string is not empty
+
+}else{
+// user is registered and string is not empty
+
+}
+
+/*else if ($text == "1") {
     // Business logic for first level response
     $response = "CON Choose account information you want to view \n";
     $response .= "1. Account number \n";
@@ -30,10 +46,10 @@ if ($text == "") {
     // This is a terminal request. Note how we start the response with END
     $response = "END Your account number is ".$accountNumber;
 
-}
+}*/
 
 // Echo the response back to the API
 header('Content-type: text/plain');
-echo $response;
+//echo $response;
 
 ?>
---
+++ menu.php
@@ -0,0 +1,36 @@
+<?php 
+class Menu{
+    protected $text;
+    protected $sessionId;
+    
+    function __construct($text, $sessionId)
+    {
+        $this->text = $text;
+        $this->sessionId = $sessionId;
+    }
+
+    public function mainMenuRegistered(){
+        $response = "CON Reply with \n";
+        $response .= "1. Send money\n";
+        $response .= "2. Withdraw\n";
+        $response .= "3. Check balance\n";
+        echo $response;
+    }
+
+    public function mainMenuUnRegistered(){
+        $response = "CON Welcome to this app. Reply with\n";
+        $response .= "1. Register\n";
+        echo $response;
+    }
+
+    public function registerMenu(){}
+
+    public function sendMoneyMenu(){}
+
+    public function withdrawMoneyMenu(){}
+
+    public function checkBalanceMenu(){}
+}
+
+
+?>
