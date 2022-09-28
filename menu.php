c0f0f6108f2edb00358b61cd3c6b8797b7a53443
martin-creator <martinlubowa@outlook.com>
2022-09-28 20:59:29 +0300

    Added send money logic to menu class

--- menu.php
+++ menu.php
@@ -47,7 +47,40 @@ class Menu{
         }
     }
 
-    public function sendMoneyMenu($textArray){}
+    public function sendMoneyMenu($textArray){
+        $level = count($textArray); 
+
+        if($level == 1){
+            echo "CON Enter mobile number of the receiver";
+        }else if($level ==2 ){
+            echo "CON Enter amount";
+        }else if($level == 3){
+            echo "CON Enter your PIN";
+        }else if ($level == 4){
+            $response = "CON Send". " " . $textArray[2] . " " . $textArray[1]. "\n";
+            $response .= "1.Confirm\n";
+            $response .= "2. Cancel \n";
+            $response .= Util::$GO_BACK . "Back\n";
+            $response .= Util::$GO_TO_MAIN_MENU . "Main Menu\n";
+            echo $response;
+        }else if($level == 5 && $textArray[4] == 1){
+            // confirm
+            //send the money
+            //check if pin is correct
+            // If you have enough funds including charges
+            echo "END Thank you for using our service";
+        }else if($level == 5 && $textArray[4] == 2){
+            //cancel
+            echo "END Thank you for using our service";
+        }else if($level == 5 && $textArray[4] == Util::$GO_BACK){
+            echo "END You have requested to go back one step - PIN ";
+        }else if($level == 5 && $textArray[4] == Util::$GO_TO_MAIN_MENU){
+            echo "END You have requested to go back to main menu ";
+        }else{
+            echo "END Invalid Entry";
+        }
+
+    }
 
     public function withdrawMoneyMenu($textArray){}
 
