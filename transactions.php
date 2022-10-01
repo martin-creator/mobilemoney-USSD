9f3577558571b02fee792c39444e56e74c77ad31
martin-creator <martinlubowa@outlook.com>
2022-10-01 11:07:57 +0300

    created send money logic

--- transactions.php
+++ transactions.php
@@ -18,11 +18,27 @@
         }
 
         public function sendMoney($pdo, $uid, $ruid, $newSenderBalance, $newReceiverBalance){
+            $pdo->setAttribute(PDO::ATTR_AUTOCOMMIT, FALSE); // we use this when updating more than one table at a time
+            try{
+                $pdo->beginTransaction();
+                $stmtT = $pdo->prepare("INSERT INTO transaction (amount, uid, ruid, ttype) values(?,?,?,?)");
+                $stmtU = $pdo->prepare("UPDATE user SET balance=? WHERE  where uid=? ");
+
+                $stmtT->execute([$this->getAmount(), $uid, $ruid, $this->getTType()]);
+                $stmtU->execute([$newSenderBalance, $uid]); //update sender balance
+                $stmtU->execute([$newReceiverBalance, $ruid]); //update reciever balance
+
+                $pdo->commit();
+
+            }catch(Exception $e){
+                $pdo->rollback();
+                return "An error was encountered";
+            }
 
         }
 
         public function withDrawCash($pdo, $uid, $aid, $newBalance){
-            
+
         }
     }
 
